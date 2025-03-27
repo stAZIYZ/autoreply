d5c74c59d31739979c48ba3e11770b8e8bc0139d
stAZIYZ <muhammadaziyzaliy@gmail.com>
2025-03-27 17:39:55 +0500

    Delete generate.py

--- generate.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-api_id = 29117903 
-api_hash = "40da3cbc8a0db7acbc23d6f9a3b7cfb4" 
-
-with TelegramClient(StringSession(), api_id, api_hash) as client:
-    session_str = client.session.save()  
-    print("Sizning string session: ", session_str)  
